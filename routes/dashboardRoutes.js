487f01f63963db75e4c574106222217fe9fb592b
abdisiismohammed <abdisiismohammed@gmail.com>
2026-08-22 19:13:38 +0300

    Add client search and see more

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const authorize = require('../middleware/roleMiddleware');
 const { getOfficeDashboard, getSuperAdminDashboard } = require('../controllers/dashboardController');
-const validateFileContent = require('../middleware/fileContentValidator');
 
 router.get('/office', protect, authorize('office_admin'), getOfficeDashboard);
 router.get('/super-admin', protect, authorize('super_admin'), getSuperAdminDashboard);
