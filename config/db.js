426043294161911996c3e530cb228a2292020ce7
abdisiismohammed <abdisiismohammed@gmail.com>
2026-08-23 09:26:04 +0300

    Fix MongoDB connection configuration

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
-    process.exit(1); // stop the app if DB connection fails
+    process.exit(1);
   }
 };
 
